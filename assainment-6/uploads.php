39d24527a78afe69bb23495f48ce490edcf178a3
MD Saidur Rahman <mdsaidurrahmanmisket@gmail.com>
2023-03-13 20:11:35 +0600

    add  assainment-6

---
+++ assainment-6/uploads.php
@@ -0,0 +1,69 @@
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body class=" vw-100 bg-dark text-light">
+    <header>
+        <!-- place navbar here -->
+    </header>
+
+
+
+
+    <main>
+        <div class="myCard">
+            <div class="cardLeft">
+                <img class="img-fluid " src="./uploads/bwIMG_5702-1.jpg" alt="">
+            </div>
+            <div class="cardRight">
+                <h1 class="text-danger">This is titlle</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus. Lorem ipsum dolor sit amet,
+                    consectetur adipisicing elit. Repudiandae natus voluptatum quas similique expedita placeat cumque
+                    alias dolorum iusto unde.</p>
+            </div>
+        </div>
+        <div class="myCard">
+            <div class="cardLeft">
+                <img class="img-fluid " src="./uploads/Bhola-The-queen-of-island.jpg" alt="">
+            </div>
+            <div class="cardRight">
+                <h1 class="text-danger">This is titlle</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus. Lorem ipsum dolor sit amet,
+                    consectetur adipisicing elit. Repudiandae natus voluptatum quas similique expedita placeat cumque
+                    alias dolorum iusto unde.</p>
+            </div>
+        </div>
+    </main>
+
+
+
+    <footer>
+        <!-- place footer here -->
+    </footer>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+        integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
